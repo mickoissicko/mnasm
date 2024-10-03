38c411a0340b96360fba658306e306d9b9811032
mickoissicko <defualtusermick@gmail.com>
2024-10-03 19:34:28 +0200

    add new fns

--- include/fn.hpp
+++ include/fn.hpp
@@ -6,6 +6,12 @@
 const std::string& FromConfig_str(const int OPT);
 const int FromConfig_num(const int OPT);
 
+int HandleSys(const char SYSTEM_COMMAND[]);
+int HandleFileI(std::ifstream& FileI);
+int HandleFileO(std::ofstream& FileO);
+int HandleChdir(const char Path[]);
+
+void HandleMntPts(char MountPointName[]);
 void InitServer(const int PORT);
 void GetConf();
 
